affbd4e5c54bda5d1ca2e8b292c718abd3a0a5e9
unknown <khansahil1886@gmail.com>
2022-06-28 22:25:08 +0530

    main.js updated

--- assets/main.js
+++ assets/main.js
@@ -3,31 +3,30 @@ function addNote() {
   const noteTitle = document.getElementById("notes__title");
   const noteDescription = document.getElementById("notes__text");
   // Validating if the inputs are empty or not.
-  if (noteTitle.value == null) {
-    return alert("Please enter the valid input!");
+  if (!noteTitle.value) {
+    return alert("Please enter the title!");
   }
-  if (noteDescription.value == null) {
-    return alert("Please enter the valid input!");
+  if (!noteDescription.value) {
+    return alert("Please enter the description!");
   }
   // Getting data from localStorage using method.
-  const notes = JSON.parse(getNote("notes"));
+  let notes = JSON.parse(getData("notes"));
+  let notesArr = [];
   if (notes === null) {
-    const noteArr = [
-      { title: noteTitle.value, description: noteDescription.value },
-    ];
-    setNote("notes");
+    notesArr = [{ title: noteTitle.value, description: noteDescription.value }];
+    setData("notes", JSON.stringify(notesArr));
   } else {
     notes.push({ title: noteTitle.value, description: noteDescription.value });
-    setNote("notes");
+    setData("notes", JSON.stringify(notes));
   }
-  noteTitle = "";
-  noteDescription = "";
+  noteTitle.value = "";
+  noteDescription.value = "";
 }
 // Function for getting the notes.
-function getNote(getNotes) {
-  return localStorage.getItem(getNotes);
+function getData(key) {
+  return localStorage.getItem(key);
 }
 // Function for setting the notes.
-function setNote(setNotes) {
-  return localStorage.setItem(setNotes);
+function setData(key, value) {
+  return localStorage.setItem(key, value);
 }
