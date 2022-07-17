dbb9b17c5de45ac06095a269477d41fe3fd9cc21
unknown <khansahil1886@gmail.com>
2022-07-17 14:28:07 +0530

    CSS fixed & JS file updated

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,6 @@
 function addNote() {
   const noteTitle = document.getElementById("notes_title")?.value;
   const noteDescription = document.getElementById("notes_text")?.value;
-
   // Validating if the inputs are empty or not.
 
   if (!noteTitle && !noteDescription) {
@@ -16,15 +15,24 @@ function addNote() {
     return alert("Please enter the description!");
   }
 
+  // setting word limit for title
+
+  if (noteTitle.length > 80) {
+    alert(
+      "Word limit exceeded in title box, only upto 80 characters can be added"
+    );
+    return;
+  }
+
   // Retreiving data from localStorage.
   let notes = JSON.parse(getData("notes")) || [];
   notes.push({
     title: noteTitle,
     description: noteDescription,
     noteId: noteUniqueId(),
+    bookmarkStatus: "",
   });
   setData("notes", JSON.stringify(notes));
-
   addNoteToScreen(noteTitle, noteDescription, noteUniqueId());
   resetForm();
 }
@@ -61,10 +69,10 @@ const totalNotesQty = document.getElementById("totalNotesQty");
 
 // Function to load tha notes
 
-function showNotes() {
+function showNotes(notes) {
   notesContainer.innerHTML = "";
   totalNotesQty.innerText = 0;
-  let notes = JSON.parse(getData("notes"));
+  // let notes = JSON.parse(getData("notes"));
   if (notes === null) {
     return;
   }
@@ -75,7 +83,7 @@ function showNotes() {
   const totalNotes = notes.length;
   totalNotesQty.innerText = `${totalNotes}`;
 }
-showNotes();
+// showNotes();
 
 // Function for current date.
 
@@ -92,10 +100,10 @@ function currentDate() {
 function addNoteToScreen(title, description, id) {
   document.getElementById("add_note_text").style.display = "none";
   const divElem = document.createElement("div");
-
+  divElem.id = id;
   // edit button operation
   let editButtonDiv = document.createElement("div");
-  editButtonDiv.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>`;
+  editButtonDiv.innerHTML = `<i class="edit_btn fa-regular fa-pen-to-square"></i>`;
   editButtonDiv.addEventListener("click", () => {
     return editNote(title, description, id);
   });
@@ -103,11 +111,18 @@ function addNoteToScreen(title, description, id) {
 
   // delete button operation
   let deleteButtonDiv = document.createElement("div");
-  deleteButtonDiv.innerHTML = `<i class="fa-regular fa-trash-can"></i>`;
+  deleteButtonDiv.innerHTML = `<i class="delete_btn fa-regular fa-trash-can"></i>`;
   deleteButtonDiv.addEventListener("click", () => {
     return deleteNote(id);
   });
   // delete button operation end
+
+  // DakTheme
+
+  // let darkModeActive = document.querySelector(".darkMode");
+  // darkModeActive.addEventListener("click", () => {
+  //   return darkMode(id);
+  // });
 
   divElem.innerHTML = `<div class="notes_list_items">
   <div class= "edit_delete">
@@ -121,7 +136,10 @@ function addNoteToScreen(title, description, id) {
       ${description}
     </div>
     <div class="note_mark_date">
-    <i class="fa-regular fa-bookmark"></i>
+    <div class="bookmark_icons">
+    <i class="fa-regular fa-bookmark" onclick="bookmarkIconVisiblity(${id})" id="regular_bookmark" style="position: absolute"></i>
+    <i class="fa-solid fa-bookmark" onclick="bookmarkIconVisiblity(${id}) id="solid_bookmark"></i>
+    </div>
     <div class="notes_view_updates" id="view_updated">
       ${currentDate()}
     </div>
@@ -132,6 +150,31 @@ function addNoteToScreen(title, description, id) {
   totalNotesQty.innerText = `${Number(totalNotesQty.innerText) + 1}`;
   notesContainer.prepend(divElem);
 }
+
+// Bookmark functionality
+
+function bookmarkIconVisiblity(idBookmark) {
+  console.log("LN148", idBookmark);
+  let regularBookmark = document.getElementById("regular_bookmark");
+  let solidBookmark = document.getElementById("solid_bookmark");
+  let notes = JSON.parse(getData("notes"));
+  for (key of notes) {
+    console.log("LN147", idBookmark, key.noteId);
+    if (idBookmark == key.noteId) {
+      if (key.bookmarkStatus) {
+        key.bookmarkStatus = false;
+        solidBookmark.style.display = "none";
+      } else {
+        key.bookmarkStatus = true;
+        regularBookmark.style.visibility = "hidden";
+      }
+    }
+  }
+  setData("notes", JSON.stringify(notes));
+  console.log("LN159", key.bookmarkStatus);
+}
+
+// Bookmark functionality ended
 
 // Function to edit the notes.
 
@@ -146,39 +189,68 @@ function editNote(title, description, id) {
 // Function to delete the notes.
 
 function deleteNote(x) {
+  if (confirm("Are you sure ? you want to delete this file")) {
+    deleteNoteConfirmation(x);
+  } else {
+    return;
+  }
+}
+
+function deleteNoteConfirmation(y) {
+  console.log("LN169", y);
   let notes = JSON.parse(getData("notes"));
-  let deleteElem = notes.findIndex((deleteId) => deleteId.noteId === x);
+  let deleteElem = notes.findIndex((deleteId) => deleteId.noteId === y);
+  if (deleteElem == -1) {
+    return;
+  }
   notes.splice(deleteElem, 1);
   setData("notes", JSON.stringify(notes));
-  showNotes();
+  document.getElementById(y).remove();
+  totalNotesQty.innerText = totalNotesQty.innerText - 1;
 }
 
 // Function to search notes
 
 function searchNotes() {
-  let searchWord = document.getElementById("search_notes").value.toUpperCase();
-  // console.log(searchWord);
   let notes = JSON.parse(getData("notes"));
-  notes.forEach((element) => {
-    let isVisible =
-      element.title.toUpperCase().includes(searchWord) ||
-      element.description.toUpperCase().includes(searchWord);
-    notes.element.classList.toggle("hide", !isVisible);
-  });
+  let filteredList = [];
+  let searchWord = document.getElementById("search_notes").value.toUpperCase();
+  for (let elem of notes) {
+    let textValue =
+      elem.title.toUpperCase().includes(searchWord) ||
+      elem.description.toUpperCase().includes(searchWord);
+    if (textValue) {
+      filteredList.push(elem);
+    }
+  }
+  showNotes(filteredList);
 }
+searchNotes();
 
 // Function dark theme
 
-function darkModeIcon() {
-  const darkModeIcon = document.querySelector(".darkMode");
-  darkModeIcon.addEventListener(("onmouse", () => {}));
-}
-
-function darkTheme() {
+let themeStatus = "";
+function darkMode() {
   const body = document.querySelector("*");
   body.classList.toggle("darkMode");
   const searchBox = document.querySelector(".search_box");
   searchBox.classList.toggle("darkMode");
   const addBtn = document.getElementById("add_btn");
   addBtn.classList.toggle("darkMode");
+  const text = document.querySelector("#notes_title, #notes_text");
+  text.classList.toggle("darkMode");
+  themeStatus = JSON.parse(getData("themeStatus")) || null;
+  if (themeStatus) {
+    themeStatus = false;
+  } else {
+    themeStatus = true;
+  }
+  setData("themeStatus", JSON.stringify(themeStatus));
 }
+
+(() => {
+  let themeStatus = JSON.parse(getData("themeStatus")) || null;
+  if (themeStatus) {
+    darkMode();
+  }
+})();
